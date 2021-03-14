b5ba5bb511db33078a15e2483038afa6963d7de6
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-03-14 11:25:24 -0300

    dobro do quadrado

---
+++ areaquadrad.py
@@ -0,0 +1,11 @@
+lado_quadrado = input('Digite o lado do quadrado: ')
+
+lado1 = eval(lado_quadrado)
+# fazemos o calculo da área do quadrado
+quadrado = lado1 * lado1 
+# dobramos o valor
+quadrado_dobro = 2 * quadrado
+
+print('O dobro da área deste quadrado é : ' , quadrado_dobro)
+
+
