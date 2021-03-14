0138c7bf27bb5f0def5b6ab81e34488603597422
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-03-14 11:17:00 -0300

    alteração perimetro

--- perimetro.py
+++ perimetro.py
@@ -1,7 +1,7 @@
 raio = input('Digite o raio: ')
 
 raio1 = eval(raio)
-
+# calcula multiplicando a medida do raio por 2 vezes e valor de pi 
 diametro = 2 * 3.14 * raio1
 
 print('O diametro da circunferência é : ' , diametro)
