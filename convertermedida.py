cf1424d8615ab2902a4a8e6000dea4df82c8b003
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-03-14 10:50:54 -0300

    converter medidas
    Converte metros em centimetros

---
+++ convertermedida.py
@@ -0,0 +1,7 @@
+medida_metros = input('Digite quantos metros são: ')
+
+metro = eval(medida_metros)
+
+converte_cem = metro * 100 
+
+print( " A mesma medida são: " , converte_cem , " centímetros.")
