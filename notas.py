a02a4741aaf19f60c1bbec9ab6f2e343929a2baf
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-03-14 10:40:53 -0300

    Media de notas

---
+++ notas.py
@@ -0,0 +1,14 @@
+nota1 = input('Digite a nota 1: ')
+nota2 = input('Digte a nota 2: ')
+nota3 = input('Digite a nota 3: ')
+nota4 = input('Digite a nota 4: ')
+
+n1 = eval(nota1)
+n2 = eval(nota2)
+n3 = eval(nota3)
+n4 = eval(nota4)
+
+# faremos a média do total de notas
+med = (n1 + n2 + n3 + n4) / 4
+
+print('A média das notas nesse Bimestre é: ' , med )
