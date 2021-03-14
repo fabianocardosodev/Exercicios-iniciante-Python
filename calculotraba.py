b146614c5ed066d743cb3bff9b57d905b54bbaee
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-03-14 11:34:20 -0300

    Calculo hora trabalhada

---
+++ calculotraba.py
@@ -0,0 +1,11 @@
+ganho_hora = input('Digite quanto ganha por hora: ')
+horas_trabalhadas = input('Digite quantos horas trabalha por mês: ')
+
+gh1 = eval(ganho_hora)
+ht1 = eval(horas_trabalhadas)
+
+sum = gh1 * ht1 
+
+print('O seu salário por mês será: ' , sum)
+
+
