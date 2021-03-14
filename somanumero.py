adf0d45ba4b2c6cb333e8df2e452ff83d24b1902
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-03-14 10:23:43 -0300

    somando 2 números

---
+++ somanumero.py
@@ -0,0 +1,9 @@
+number_1 = input('Digite o  primeiro número: ')
+number_2 = input('Digite o segundo número: ')
+
+num1 = eval(number_1)
+num2 = eval(number_2)
+
+sum = num1 + num2
+
+print('A soma dos dois números é: ' , sum )
