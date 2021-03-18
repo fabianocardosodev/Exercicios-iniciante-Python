ceb864759a848f5569b776d37a414b9421a25fd5
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-03-18 13:34:22 -0300

    digita num positivo

---
+++ numpositivo.py
@@ -0,0 +1,9 @@
+num = eval(input('Digite um numero positivo: '))
+
+
+while num < 0:
+    num = eval (input('Digite um numero positivo: ')) 
+    if num >= 0 :
+        print('Você acertou!')
+print('Fim do jogo!')
+
