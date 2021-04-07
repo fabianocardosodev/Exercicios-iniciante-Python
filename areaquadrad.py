7441b6cff5143414f2759ee02146d92f8d5fa7f4
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-04-06 23:28:09 -0300

    alteração

--- areaquadrad.py
+++ areaquadrad.py
@@ -1,7 +1,7 @@
 lado_quadrado = input('Digite o lado do quadrado: ')
 
 lado1 = eval(lado_quadrado)
-# fazemos o calculo da área do quadrado
+# fazer o calculo da área do quadrado
 quadrado = lado1 * lado1 
 # dobramos o valor
 quadrado_dobro = 2 * quadrado
